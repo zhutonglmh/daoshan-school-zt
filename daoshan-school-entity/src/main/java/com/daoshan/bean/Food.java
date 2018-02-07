3c94fa3edb9b6d5bb06dbfa9df8898f54aeb3774
zhutong <zhutonglmh@163.com>
2018-02-07 13:55:05 +0800

    部署完成 日志与druid

---
+++ daoshan-school-entity/src/main/java/com/daoshan/bean/Food.java
@@ -0,0 +1,13 @@
+package com.daoshan.bean;
+
+public class Food {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ daoshan-school-web/src/main/java/com/daoshan/school/DaoshanSchoolWebApplication.java
@@ -0,0 +1,17 @@
+package com.daoshan.school;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
+public class DaoshanSchoolWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoshanSchoolWebApplication.class, args);
+	}
+}
