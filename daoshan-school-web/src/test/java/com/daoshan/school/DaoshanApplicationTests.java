d1c96cd090a106f0815c8817ef8d259871689e44
zhutong <zhutonglmh@163.com>
2019-01-09 21:22:26 +0800

    删除掉缓存等配置,下载即可跑

--- daoshan-school-web/src/test/java/com/daoshan/school/DaoshanApplicationTests.java
+++ daoshan-school-web/src/test/java/com/daoshan/school/DaoshanApplicationTests.java
@@ -11,36 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DaoshanApplicationTests {
 
-	public static final Logger LOG = LoggerFactory.getLogger(DaoshanApplicationTests.class);
-
 	@Test
-	public void contextLoads() {
-
-		try {
-//			String str = null;
-//			str.toCharArray();
-           //AliSmsUntils.sms("15689438669","2222");
-
-            //EmailSend.send_mail("shandongsunzhi@126.com","000099");
-			return;
-		}catch (NoClassDefFoundError e){
-			System.out.println(e);
-			LOG.error("测试出现类未发现异常日志 ,errMsg = {}, stack info = {}",e.getMessage(),e.toString());
-		}catch (StringIndexOutOfBoundsException e){
-			System.out.println(e);
-			LOG.error("测试出现角标越界异常日志 ,errMsg = {}, stack info = {}",e.getMessage(),e.toString());
-		}catch (NullPointerException e){
-			System.out.println(e);
-			LOG.error("测试出现空指针异常日志 ,errMsg = {}, stack info = {}",e.getMessage(),e.toString());
-		}catch (Exception e){
-			System.out.println(e);
-			LOG.error("测试出现异常日志 ,errMsg = {}, stack info = {}",e.getMessage(),e.toString());
-		}
-		finally {
-
-		}
-	}
-
-
+	public void contextLoads() {}
 
 }
